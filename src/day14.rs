40be7d9cb9a609f89fc80c6acaff6e3f83fe1f7f
Yiğit Dinçer <yigitdincer1@gmail.com>
2023-12-30 22:05:31 +0100

    Solution for Day 14 Part 2 in progress

--- src/day14.rs
+++ src/day14.rs
@@ -43,26 +43,34 @@ fn tilt(platform: &Platform, direction: Direction) -> Platform {
     let width = platform.first().unwrap().len();
     let height = platform.len();
 
-    let mut changed = true;
+    for y in 0..height {
+        for x in 0..width {
+            if tilted_platform[y][x] == Tile::Rock {
+                let mut last_x = x;
+                let mut last_y = y;
 
-    while (changed) {
-        changed = false;
-        for y in 0..height {
-            for x in 0..width {
-                let value = tilted_platform[y][x];
+                for i in 1.. {
+                    let new_x = x as i32 + i * move_vector.0;
+                    let new_y = y as i32 + i * move_vector.1;
 
-                if value == Tile::Rock {
-                    let new_x = (x as i32 + move_vector.0) as usize;
-                    let new_y = (y as i32 + move_vector.1) as usize;
-
-                    if new_x >= width || new_y >= height {
-                        continue;
+                    if new_x < 0 || new_x >= width as i32 || new_y < 0 || new_y >= height as i32 {
+                        if last_x != x || last_y != y {
+                            tilted_platform[last_y][last_x] = Tile::Rock;
+                            tilted_platform[y][x] = Tile::Empty;
+                        }
+                        break;
                     }
 
-                    if tilted_platform[new_y][new_x] == Tile::Empty {
-                        tilted_platform[new_y][new_x] = Tile::Rock;
-                        tilted_platform[y][x] = Tile::Empty;
-                        changed = true;
+                    if tilted_platform[new_y as usize][new_x as usize] == Tile::Empty {
+                        last_x = new_x as usize;
+                        last_y = new_y as usize;
+                        continue;
+                    } else {
+                        if last_x != x || last_y != y {
+                            tilted_platform[last_y][last_x] = Tile::Rock;
+                            tilted_platform[y][x] = Tile::Empty;
+                        }
+                        break;
                     }
                 }
             }
@@ -96,9 +104,35 @@ fn solve_1(input: &str) -> usize {
     get_total_load(&tilt(&parse_into_platform(&input), Direction::North))
 }
 
+fn tilt_one_cycle(platform: &Platform) -> Platform {
+    let mut initial_tilt = platform.clone();
+    initial_tilt = tilt(&initial_tilt, Direction::North);
+    initial_tilt = tilt(&initial_tilt, Direction::West);
+    initial_tilt = tilt(&initial_tilt, Direction::South);
+    initial_tilt = tilt(&initial_tilt, Direction::East);
+    initial_tilt
+}
+
+fn solve_2(input: &str) -> usize {
+    let mut initial_tilt = parse_into_platform(&input);
+    let mut last_platform: Platform = initial_tilt.clone();
+
+    for _ in 1..1000000000 {
+        initial_tilt = tilt_one_cycle(&initial_tilt);
+
+        if last_platform != initial_tilt {
+            last_platform = initial_tilt.clone();
+        } else {
+            break;
+        }
+    }
+
+    get_total_load(&initial_tilt)
+}
+
 pub fn solve(input: &str) {
     println!("{}", solve_1(input));
-    //println!("{}", solve_2(input));
+    println!("{}", solve_2(input));
 }
 
 #[cfg(test)]
@@ -167,9 +201,25 @@ O..#.OO...
 #....###..
 #....#....";
 
+    const EXAMPLE_INPUT_AFTER_1_CYCLE: &str = ".....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....";
+
     #[test]
     fn solve_1() {
         assert_eq!(super::solve_1(&EXAMPLE_INPUT), 136);
+    }
+
+    #[test]
+    fn solve_2() {
+        assert_eq!(super::solve_2(&EXAMPLE_INPUT), 64);
     }
 
     #[test]
@@ -199,6 +249,14 @@ O..#.OO...
                 Direction::North
             ),
             super::parse_into_platform(&TILTED_EXAMPLE_INPUT)
+        );
+    }
+
+    #[test]
+    fn tilt_1_cycle() {
+        assert_eq!(
+            super::tilt_one_cycle(&super::parse_into_platform(&EXAMPLE_INPUT)),
+            super::parse_into_platform(&EXAMPLE_INPUT_AFTER_1_CYCLE)
         );
     }
 
